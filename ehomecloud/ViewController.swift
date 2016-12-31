0afbd90f6d952a48c6bf3829e57ba5e1d3f73108
付宝阳 <9622777@qq.com>
2016-12-31 19:49:27 +0800

    Initial Commit

---
+++ ehomecloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ehomecloud
+//
+//  Created by eastsoft_fuby on 2016/12/31.
+//  Copyright © 2016年 eastsoft_fuby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
